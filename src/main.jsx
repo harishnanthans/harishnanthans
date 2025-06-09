fed3afc66a68f45817d95f13b5636e37cf125bca
harishns <harishns@tekion.com>
2025-06-09 10:47:42 +0530

    added navbar path into main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import Home from './pages/home/Home.jsx'
-import NavBar from './components/Navbar.jsx'
+import NavBar from './components/NavBar.jsx'
 
 import "./index.scss"
 
